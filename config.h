b321f153330c3cabe8f04ead7da6cda91dd965f1
Aleksey Nikitin <imsouz9@gmail.com>
2022-04-07 22:39:17 +0300

    Adjust cursor color

--- config.h
+++ config.h
@@ -118,7 +118,7 @@ static const char *colorname[] = {
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
+	"#888888",
 	"#555555",
 };
 
@@ -130,7 +130,7 @@ static const char *colorname[] = {
 unsigned int defaultfg = 7;
 unsigned int defaultbg = 0;
 unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
